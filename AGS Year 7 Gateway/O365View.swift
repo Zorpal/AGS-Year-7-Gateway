05121eb605ac39ce9af2c45f3218dba693062c5d
Zorpal <inz1512006@gmail.com>
2025-03-26 14:24:22 +0000

    AGS SD Version 2

---
+++ AGS Year 7 Gateway/O365View.swift
@@ -0,0 +1,18 @@
+//
+//  O365View.swift
+//  AGS Year 7 Gateway
+//
+//  Created by AGS Software Dev 1 on 26/03/2025.
+//
+
+import SwiftUI
+
+struct O365View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    O365View()
+}
